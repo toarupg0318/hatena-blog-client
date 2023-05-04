1aad2ac142759aafc1dbdccb739271f730ccfdd8
toarupi0318 <toarupg0318@gmail.com>
2023-05-04 19:06:18 +0900

    add: tests throws exception if passed entryId is empty

---
+++ tests/Unit/HatenaClientTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Toarupg0318\HatenaBlogClient\Exceptions\HatenaInvalidArgumentException;
+use Toarupg0318\HatenaBlogClient\HatenaClient;
+
+$hatenaClient = HatenaClient::getInstance('hoge0123', 'fuga0987');
+
+it('tests getPostByEntryId() throws exception if passed entryId is empty.', function () use ($hatenaClient) {
+    expect(fn () => $hatenaClient->getPostByEntryId(''))
+        ->toThrow(
+            exception: HatenaInvalidArgumentException::class,
+            exceptionMessage: 'Entry id is empty.'
+        );
+});
+
+it('tests deletePostByEntryId() throws exception if passed entryId is empty.', function () use ($hatenaClient) {
+    expect(fn () => $hatenaClient->deletePostByEntryId(''))
+        ->toThrow(
+            exception: HatenaInvalidArgumentException::class,
+            exceptionMessage: 'Entry id is empty.'
+        );
+});
