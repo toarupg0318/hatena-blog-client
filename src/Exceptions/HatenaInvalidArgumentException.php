79d3ac63baa1ff90990d1a7cc014dcd24d937a49
toarupi0318 <toarupg0318@gmail.com>
2023-05-04 19:02:16 +0900

    add: HatenaInvalidArgumentException

---
+++ src/Exceptions/HatenaInvalidArgumentException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Toarupg0318\HatenaBlogClient\Exceptions;
+
+use Throwable;
+
+final class HatenaInvalidArgumentException extends HatenaException
+{
+    const MESSAGE = 'Passed argument is invalid.';
+
+    public function __construct(
+        string $message = self::MESSAGE,
+        int $code = 0,
+        null|Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
