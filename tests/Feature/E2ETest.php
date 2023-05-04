af72d76a0cc21421918d66d031bc9445e0786ee3
toarupi0318 <toarupg0318@gmail.com>
2023-05-04 21:04:27 +0900

    prepare: e2e test

--- tests/Feature/E2ETest.php
+++ tests/Feature/E2ETest.php
@@ -1,35 +1,36 @@
 <?php
 
-// init env
+use Dotenv\Dotenv;
 use Toarupg0318\HatenaBlogClient\HatenaClient;
 
-$env = \Dotenv\Dotenv::createUnsafeImmutable(
-    __DIR__ . '/../..',
-    '.env'
-);
-$env->safeLoad();
-
-$hatenaId = getenv('HATENA_ID');
-$hatenaApiKey = getenv('HATENA_API_KEY');
-
-// todo: ブログエントリの一覧取得
-//// todo: getParsedData
-//// todo: getFirstPageUrl
-//// todo: getNextPageUrl
-//if (
-//    !is_string($hatenaId) || !is_string($hatenaApiKey) ||
-//    empty($hatenaId) || empty($hatenaApiKey)
-//) {
-//    throw new Exception('Secrets required for test is not loaded correctly.');
-//}
+//// init env
+//$env = Dotenv::createUnsafeImmutable(
+//    __DIR__ . '/../..',
+//    '.env'
+//);
+//$env->safeLoad();
+//
+//$hatenaId = getenv('HATENA_ID');
+//$hatenaApiKey = getenv('HATENA_API_KEY');
 //
 //$hatenaClient = HatenaClient::getInstance($hatenaId, $hatenaApiKey);
 //
-//it('tests get list feature.', function () use ($hatenaClient) {
-//    $hatenaClient->getList();
+//it('ensures straight through test connects to Hatena with using actual account', function () {
+//    // post a new blog entry, called as "registered-entry" below.
+//    sleep(1);
+//
+//    // get list, confirm registered-entry is contained.
+//    sleep(1);
+//
+//    // get the registered-entry, confirm registered-entry is fetched correctly.
+//    sleep(1);
+//
+//    // edit the registered-entry, confirm registered-entry is edited correctly.
+//    sleep(1);
+//
+//    // delete the registered-entry
+//    sleep(1);
+//
+//    // get the registered-entry, confirm registered-entry is not found.
+//    sleep(1);
 //});
-
-// todo: ブログエントリの取得
-// todo: ブログエントリの投稿
-// todo: ブログエントリの編集
-// todo: ブログエントリの削除
