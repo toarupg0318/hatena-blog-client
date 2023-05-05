d1ede885e9a176347c73f4813cfe67acf28f5d57
toarupi0318 <toarupg0318@gmail.com>
2023-05-05 22:52:41 +0900

    add: FootNote class

---
+++ src/ValueObjects/FootNote.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Toarupg0318\HatenaBlogClient\ValueObjects;
+
+final class FootNote
+{
+    public readonly string $vocabulary;
+
+    public function __construct(
+        string $vocabulary,
+        public readonly string $description
+    ) {
+        $this->vocabulary = trim($vocabulary, " \t\n\r\0\x0B" . '　');
+    }
+}
