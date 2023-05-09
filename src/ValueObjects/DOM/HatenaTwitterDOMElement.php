d0e61ab5abfbc65f532017a9ae21b0ba51b41542
toarupi0318 <toarupg0318@gmail.com>
2023-05-10 08:58:03 +0900

    add: HatenaTwitterDOMElement and its test

---
+++ src/ValueObjects/DOM/HatenaTwitterDOMElement.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Toarupg0318\HatenaBlogClient\ValueObjects\DOM;
+
+use Toarupg0318\HatenaBlogClient\Exceptions\HatenaUnexpectedException;
+
+final class HatenaTwitterDOMElement extends HatenaDOMElement
+{
+    /**
+     * "status" means something like tweet id in Twitter.
+     * For example, The status of "https://twitter.com/toarupg0318/status/1640997727260794882"
+     * is "1640997727260794882"
+     *
+     * @param string $status
+     */
+    public function __construct(
+        public readonly string $status
+    ) {
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return "twitter:{$this->status}:tweet" . PHP_EOL;
+    }
+
+    /**
+     * @param string $httpsValue
+     * @return self
+     * @throws HatenaUnexpectedException
+     */
+    public static function createFromHttpsValue(string $httpsValue): self
+    {
+        preg_match('/^(https:\/\/twitter\.com\/)(.*)(\/status\/)(.*)$/u', $httpsValue, $matches);
+        if (array_key_exists(4, $matches) && is_string($matches[4])) {
+            return new self($matches[4]);
+        }
+
+        throw new HatenaUnexpectedException('Twitter URL is invalid.');
+    }
+}
