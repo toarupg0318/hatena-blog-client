b8ee1337563fa6cada3eacb6f5d4c4d8823a7f81
toarupi0318 <toarupg0318@gmail.com>
2023-05-10 09:03:09 +0900

    fix: add PHP_EOL to HatenaTwitterDOMElementTest

--- tests/Unit/ValueObjects/HatenaTwitterDOMElementTest.php
+++ tests/Unit/ValueObjects/HatenaTwitterDOMElementTest.php
@@ -4,16 +4,15 @@ use Toarupg0318\HatenaBlogClient\Exceptions\HatenaUnexpectedException;
 use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaTwitterDOMElement;
 
 it('tests HatenaTwitterDOMElement performs correctly.', function () {
-    // https://twitter.com/toarupg0318/status/1640997727260794882
     $testTwitterStatus = '1640997727260794882';
     expect(strval(new HatenaTwitterDOMElement($testTwitterStatus)))
-        ->toBe('twitter:1640997727260794882:tweet');
+        ->toBe('twitter:1640997727260794882:tweet' . PHP_EOL);
 });
 
 it('tests createFromHttpsValue() performs correctly.', function () {
     $testTwitterURL = 'https://twitter.com/toarupg0318/status/1640997727260794882';
     expect(strval(HatenaTwitterDOMElement::createFromHttpsValue($testTwitterURL)))
-        ->toBe('twitter:1640997727260794882:tweet');
+        ->toBe('twitter:1640997727260794882:tweet' . PHP_EOL);
 });
 
 it('tests if passed param is invalid, createFromHttpsValue() throws Exception correctly.', function () {
