079ae8f600082b424d5615a58a288f79c87bd25b
toarupi0318 <toarupg0318@gmail.com>
2023-05-13 15:51:56 +0900

    add: some append methods to HatenaDOMDocument

--- src/ValueObjects/HatenaDOMDocument.php
+++ src/ValueObjects/HatenaDOMDocument.php
@@ -8,10 +8,16 @@ use Countable;
 use Generator;
 use IteratorAggregate;
 use Stringable;
+use Toarupg0318\HatenaBlogClient\Exceptions\HatenaInvalidArgumentException;
 use Toarupg0318\HatenaBlogClient\Exceptions\HatenaUnexpectedException;
 use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\FootNoteAttachable;
+use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaBlockQuoteDOMElement;
+use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaBrDOMElement;
 use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaDOMElement;
+use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaDtDOMElement;
 use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaH3DOMElement;
+use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaH4DOMElement;
+use Toarupg0318\HatenaBlogClient\ValueObjects\DOM\HatenaH5DOMElement;
 
 /**
  * @implements IteratorAggregate<HatenaDOMElement>
@@ -56,11 +62,66 @@ final class HatenaDOMDocument implements IteratorAggregate, Stringable, Countabl
     /**
      * @param string $h3Value
      * @return self
-     * @throws HatenaUnexpectedException
+     * @throws HatenaUnexpectedException|HatenaInvalidArgumentException
      */
     public function appendH3(string $h3Value): self
     {
         return $this->append(new HatenaH3DOMElement($h3Value));
+    }
+
+    /**
+     * @param string $h4Value
+     * @return self
+     * @throws HatenaInvalidArgumentException|HatenaUnexpectedException
+     */
+    public function appendH4(string $h4Value): self
+    {
+        return $this->append(new HatenaH4DOMElement($h4Value));
+    }
+
+    /**
+     * @param string $h5Value
+     * @return self
+     * @throws HatenaUnexpectedException|HatenaInvalidArgumentException
+     */
+    public function appendH5(string $h5Value): self
+    {
+        return $this->append(new HatenaH5DOMElement($h5Value));
+    }
+
+    /**
+     * @param string $value
+     * @param string|null $url
+     * @return self
+     * @throws HatenaUnexpectedException
+     */
+    public function appendBlockQuote(
+        string $value,
+        string $url = null
+    ): self {
+        return $this->append(new HatenaBlockQuoteDOMElement($value, $url));
+    }
+
+    /**
+     * @return self
+     * @throws HatenaUnexpectedException
+     */
+    public function appendBr(): self
+    {
+        return $this->append(new HatenaBrDOMElement());
+    }
+
+    /**
+     * @param string $title
+     * @param string $description
+     * @return self
+     * @throws HatenaUnexpectedException
+     */
+    public function appendDt(
+        string $title,
+        string $description
+    ): self {
+        return $this->append(new HatenaDtDOMElement($title, $description));
     }
 
     /**
